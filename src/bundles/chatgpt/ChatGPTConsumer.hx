b6971a6980c46d226d2a3ebd0f35a58dfa0c1f95
Ian Harrigan <ianharrigan@hotmail.com>
2023-07-21 11:45:07 +0200

    skeleton

---
+++ src/bundles/chatgpt/ChatGPTConsumer.hx
@@ -0,0 +1,22 @@
+package bundles.chatgpt;
+
+import esb.core.IBundle;
+import esb.common.Uri;
+import esb.core.IConsumer;
+import esb.logging.Logger;
+import esb.core.Bus.*;
+import promises.Promise;
+
+@:keep
+class ChatGPTConsumer implements IConsumer {
+    private static var log:Logger = new Logger("bundles.chatgpt.ChatGPTConsumer");
+
+    public var bundle:IBundle;
+    public function start(uri:Uri) {
+        log.info('creating chatgpt consumer for ${uri.toString()}');
+        from(uri, message -> {
+            return new Promise((resolve, reject) -> {
+            });
+        });
+    }
+}
